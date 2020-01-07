eca1f8b966a50d9a611b615bce6cc13b43034ca2
chenfengami <18094159624@163.com>
2020-01-07 23:26:24 +0800

    chore 开发环境配置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [AppService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-cli';
+
+  constructor(
+    private appService: AppService
+  ) {}
+
+  ngOnInit() {
+    this.appService.getUsers()
+      .subscribe((res: any) => {
+        console.log('users', res);
+      });
+  }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AppService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUsers(): Observable<any> {
+    return this.http.get('/api/users');
+  }
+}
